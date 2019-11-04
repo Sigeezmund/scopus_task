133822e00bcfcdd718ebb6edd1c0efc32f920976
Peretyatko Oleg <o.peretyatko@qiwi.com>
2019-11-04 20:46:09 +0300

    Delete unused dependecy

--- src/main/java/ScopusTask.java
+++ src/main/java/ScopusTask.java
@@ -8,7 +8,9 @@ import java.util.List;
 public class ScopusTask {
 
     public static void main(String[] args) {
+
         List<Long> authorsId = new ArrayList<Long>(Arrays.asList(7005686056L, 7005686057L, 7005686070L, 7005686088L, 7005686080L));
+
         WorkWithIdService workWithIdService = new WorkWithIdService(new HttpInfoServiceImpl());
         workWithIdService.workWithAuthors(authorsId);
     }
